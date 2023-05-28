928d0d08deb6b47f0e5679854461281aa0ca6a00
Bhushan Pakhle <dhruvpakhle4@gmail.com>
2023-05-28 13:00:21 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -23,19 +23,18 @@ const imageAltText = "graphs";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a third year Computer Engineering student studying at Fr. CRCE,Bandra. I like maths and statistics and also have passion for data analysis.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Web development",
+  "Data Structures and Algorithms",
+  "Data Analysis",
+  "Cloud Computing",
+  "Machine Learning",
 ];
 
 /**
@@ -44,7 +43,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation.";
 
 const About = () => {
   return (
