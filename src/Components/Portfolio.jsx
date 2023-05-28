1b68c557fe2a5523e2417ca0f62aa9abc2fd9940
Bhushan Pakhle <dhruvpakhle4@gmail.com>
2023-05-28 12:40:59 +0530

    Update Portfolio.jsx with my projects

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -30,28 +30,24 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
-    description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+    title: "LifeStream",
+    description: "Blood Donation Portal.",
+    url: "https://lifestream-blood-donation-website.bhushanpakhle.repl.co/",
   },
   {
-    title: "Web Development for Beginners",
-    description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+    title: "TickEth",
+    description: "Ticket purchase and management platform based on Ethereum.",
+    url: "https://github.com/bhushanpakhle12/Team_FUTSOL_TickETH",
   },
   {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+    title: "AI-web-app-for-Object-Detection",
+    description: "Object Detection using webcam.",
+    url: "https://github.com/bhushanpakhle12/AI-web-app-for-Object-Detection",
   },
   {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+    title: "Cloud_Computing_Project",
+    description: "Mini project deployment for Cloud Computing subject.",
+    url: "https://github.com/bhushanpakhle12/Cloud_Computing_Mini_Project",
   },
 ];
 
