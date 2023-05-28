ed4135cf971297a6c00f1f2286da3ae6a1f35cd1
Bhushan Pakhle <dhruvpakhle4@gmail.com>
2023-05-28 12:36:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Bhushan Pakhle",
+  title: "Data Analysis and ML Enthusiast",
+  email: "dhruvpakhle4@gmail.com",
+  gitHub: "bhushanpakhle12",
+  instagram: "bhushanpakhle",
+  linkedIn: "bhushan-pakhle",
   medium: "",
-  twitter: "microsoft",
+  twitter: "",
   youTube: "Code",
 };
 
